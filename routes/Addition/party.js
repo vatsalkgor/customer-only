601db717ef38e6c08e1b68a93e8e6e1fb0999acd
Vatsal Gor <vatsalkgor@gmail.com>
2020-03-29 17:04:35 +1300

    party show code added not tested

--- routes/Addition/party.js
+++ routes/Addition/party.js
@@ -2,9 +2,33 @@ const Party = require("express").Router();
 const db = require("../../db");
 
 Party.get("/", (req, res) => {
-  res.render("addition/party");
+  let data=[];
+  db.all('select * from party',(err,rows)=>{
+    if(err){
+      console.log(err)
+    }
+    console.log(rows)
+    rows.forEach((row)=>{
+      data.push({row})
+    })
+  })
+  console.log(data)
+  res.render("addition/party",{data});
 });
 
-Party.post("/Add", (req, res) => {});
+Party.post("/Add", (req, res) => {
+  let data=[];
+  db.run('insert into party (p_name,p_pending_amount) VALUES(?,?)',[req.body.p_name,req.body.p_amount],(err)=>{
+    if(err){
+      console.log(err);
+    }
+    db.all('select * from party',(err,rows)=>{
+      rows.forEach((row)=>{
+        data.push({row})
+      })
+      res.send({success:true,msg:'Party Added.',data})
+    })
+  })
+});
 
 module.exports = Party;
